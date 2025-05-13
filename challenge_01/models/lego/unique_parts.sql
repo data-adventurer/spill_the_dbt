d876aa527417bf67a675d1f66b292aad0fbb54ad
Lorraine Ferrusi <lferrusi94@gmail.com>
2025-05-13 13:57:10 -0400

    Commit Challenge 02 for the spillthedbt series

---
+++ challenge_01/models/lego/unique_parts.sql
@@ -0,0 +1,8 @@
+select 
+	p.part_num
+from parts as p
+inner join inventory_parts as ip on p.part_num = ip.part_num
+inner join inventories as i on i.id = ip.inventory_id
+inner join sets as s on s.set_num = i.set_num
+	group by p.part_num
+	having count(*) = 1
